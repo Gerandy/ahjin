ac3c7c9722bf4ac901e5734a45d2d8101c1e99ac
weng <ic.erwin.vallez@cvsu.edu.ph>
2025-04-26 04:10:33 +0800

    we

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -75,20 +75,12 @@ export const About = () => {
               <div>Connect with me 
                  <a href="#fb" className="ml-5 ">
                       <button
+                        img src="src/assets/john profile.jpg"
                         type="button"
                         data-twe-ripple-init
                         data-twe-ripple-color="light"
                         className="mb-2 inline-block rounded bg-[#1877f2] hover:scale-110 px-2 py-2 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-                        <span class="[&>svg]:h-5 [&>svg]:w-5">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 320 512">
-
-                            <path
-                              d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-                          </svg>
-                        </span>
+                        
                       </button>
                   </a>
               </div>
