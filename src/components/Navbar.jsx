156fd828e555379e0ed150fd8d1542c56af67f62
weng <ic.erwin.vallez@cvsu.edu.ph>
2025-04-26 05:18:16 +0800

    nav bar animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,26 +13,32 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                     {" "}
-                    <div className="flex justify-between items-center"><img className="h-12 w-12 mr-2" src="src/assets/ahjinLogo.png"></img>Ahjin<span className="text-purple-500">.guild</span></div>                
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center group cursor-pointer">
+                          <img className="h-12 w-12 mr-2 group-hover:scale-105" src="src/assets/ahjinLogo.png" alt="logo" />
+                          <span className="text-gray-300 group-hover:text-purple-500 transition-colors duration-400 group-hover:scale-105 mr-1">Ahjin</span>
+                          <span className="text-purple-500 group-hover:text-gray-300 transition-colors duration-400 group-hover:scale-105">.guild</span>
+                        </div>
+                    </div>                
                 </a>
                 <div className="w-7 h5 relative cursor-pointer z-40 md:hidden hover:scale-110" onClick={() => setMenuOpen((prev) => !prev)}>
                   &#9776;
                 </div>
 
                 <div className="hidden md:flex items-center space-x-8">
-                  <a href="#home" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-300 hover:after:w-full">
+                  <a href="#home" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-100 hover:after:w-full">
                     {""}
                     Home{""}
                   </a>
-                  <a href="#about" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-300 hover:after:w-full">
+                  <a href="#about" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-100 hover:after:w-full">
                     {""}
                     About{""}
                   </a>
-                  <a href="#projects" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-300 hover:after:w-full">
+                  <a href="#projects" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-100 hover:after:w-full">
                     {""}
                     Project{""}
                   </a>
-                  <a href="#contact" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-300 hover:after:w-full">
+                  <a href="#contact" className="relative text-gray-300 hover:text-purple-400 transition-colors hover:scale-110 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-purple-400 after:transition-all after:duration-100 hover:after:w-full">
                     {""}
                     Contacts{""}
                   </a>
